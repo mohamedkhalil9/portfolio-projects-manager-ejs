0235f8b5c7e1c6d4e46bb4c3cb0e138fad5f3067
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-07-01 15:11:52 +0300

    Add: edit profile view to the controller

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -5,7 +5,11 @@ import jwt from "jsonwebtoken";
 import { sendVerifyEmail } from "../utils/sendEmail.js";
 import { v2 as cloudinary } from "cloudinary";
 import { verifyToken } from "../utils/verifyToken.js";
+import * as factory from "./factory.js";
 
+export const getProfile = factory.getOne(User, "currentUser");
+export const updateProfile = factory.updateOne(User, "currentUser");
+export const deleteProfile = factory.deleteOne(User, "currentUser");
 export const getUserProfile = asyncHandler(async (req, res) => {
   const { id } = req.user;
   const user = await User.findById(id).select("-password");
@@ -13,15 +17,24 @@ export const getUserProfile = asyncHandler(async (req, res) => {
   res
     .status(200)
     // .json({ status: "success", data: user });
-    .render("profile/home", { user });
+    .render("profile/view-profile-2", { user });
 });
 
+export const editProfileView = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const user = await User.findById(id).select("-password");
+  if (!user) throw new AppError("user not found", 404);
+  res.render("profile/edit-profile-2", { user });
+});
 export const updateUserProfile = asyncHandler(async (req, res) => {
   const { id } = req.user;
   const userData = req.body;
   const user = await User.findByIdAndUpdate(id, userData, { new: true });
   if (!user) throw new AppError("user not found", 404);
-  res.status(200).json({ status: "success", data: user });
+  res
+    .status(200)
+    // .json({ status: "success", data: user });
+    .redirect("/api/v1/profile/edit");
 });
 
 export const deleteUserProfile = asyncHandler(async (req, res) => {
